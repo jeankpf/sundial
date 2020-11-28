70a83b6e6023c15bae48fcda987c9093f79da1a6
Jean Kempf <jean.kempf@student.isae-supaero.fr>
2020-11-28 12:00:25 +0100

    development for readibility

---
+++ README.md
@@ -0,0 +1,10 @@
+# Do you want to build a solar clock or to be able to know the real time according to the position of the Sun?
+Don't wait anymore and check for both answers thanks to this repository!
+
+## What do you need to use this script?
+You will need the [Orekit](https://www.orekit.org/){:target="\_blank"} Wrapper developped and maintained by Petrus Hyvönen who works for the [Swedish Space Corporation](https://www.sscspace.com/){:target="\_blank"} and that you can find [here!](https://gitlab.orekit.org/orekit-labs/python-wrapper){:target="\_blank"}.
+
+Please follow the instructions described in this wiki [here!](https://gitlab.orekit.org/orekit-labs/python-wrapper/-/wikis/installation){:target="\_blank"}  before using this script (I would recommend you to use the conda packages as it is way easier!)
+
+### Notes
+The mural sundial version is not working yet, it seems there is a problem in this last change of reference frame
--- mural_sundial.py
+++ mural_sundial.py
@@ -1,9 +1,6 @@
-#### Programme ne marche pas pour le mural, impression que le resultat doit etre tourner de 180 deg, a verifier,
-
-
-
+from math import radians, degrees, tan, pi
+from sundial import Sundial, get_elevation, get_azimuth
 import orekit
-import numpy
 
 vm = orekit.initVM()
 
@@ -11,21 +8,49 @@ from orekit.pyhelpers import setup_orekit_curdir
 
 setup_orekit_curdir()
 
-from org.orekit.frames import Frame, FramesFactory, TopocentricFrame, Transform
-from org.orekit.bodies import OneAxisEllipsoid, GeodeticPoint, CelestialBodyFactory
-from org.orekit.propagation.analytical.tle import TLE, TLEPropagator
-from org.orekit.utils import IERSConventions, Constants, PVCoordinatesProvider, AngularCoordinates
+from org.orekit.frames import Frame, FramesFactory, Transform
+# from org.orekit.bodies import OneAxisEllipsoid, GeodeticPoint, CelestialBodyFactory
+from org.orekit.utils import AngularCoordinates
 from org.hipparchus.geometry.euclidean.threed import Vector3D, Rotation, RotationConvention
-from org.orekit.time import AbsoluteDate, TimeScalesFactory
+from org.orekit.time import AbsoluteDate
 
-from math import radians, degrees, tan, cos, sin, pi
-
-from sundial import Sundial, UTC2LocalTime, localTime2UTC, get_elevation, get_azimuth
 
 class MuralSundial(Sundial):
+    """
+    """
 
-    def __init__(self, lat=48.58, longi=7.75, alt=142.0, loc="Strasbourg", timeZone=1, gnomonLength=1.0, mural_angle=45.0, height= 1.0, orient="Nord"):
-        super().__init__(lat, longi, alt, loc, timeZone, gnomonLength)
+    def __init__(self, lat=48.58, longi=7.75, alt=142.0, loc="Strasbourg", time_zone=1,
+                 gnomon_length=1.0, mural_angle=45.0, height=1.0, orient="Nord"):
+        """
+        
+
+        Parameters
+        ----------
+        lat : float, optional
+            DESCRIPTION. The default is 48.58.
+        longi : float, optional
+            DESCRIPTION. The default is 7.75.
+        alt : float, optional
+            DESCRIPTION. The default is 142.0.
+        loc : str, optional
+            DESCRIPTION. The default is "Strasbourg".
+        time_zone : int, optional
+            DESCRIPTION. The default is 1.
+        gnomon_length : float, optional
+            DESCRIPTION. The default is 1.0.
+        mural_angle : float, optional
+            DESCRIPTION. The default is 45.0.
+        height : float, optional
+            DESCRIPTION. The default is 1.0.
+        orient : str, optional
+            DESCRIPTION. The default is "Nord".
+
+        Returns
+        -------
+        None.
+
+        """
+        super().__init__(lat, longi, alt, loc, time_zone, gnomon_length)
         self.mural_angle = radians(mural_angle)
         self.height = height
         if orient == "Nord":
@@ -48,9 +73,18 @@ class MuralSundial(Sundial):
         xm_rot_ac = AngularCoordinates(xm_rotation, Vector3D(0.0, 0.0, 0.0))
         xm_rot_tf = Transform(whenever, xm_rot_ac)
         sundial_transform = Transform(whenever, mural_transform, xm_rot_tf)
-        self.sundial_frame = Frame(self.stationFrame, sundial_transform, "Sundial Frame")
+        self.sundial_frame = Frame(self.station_frame, sundial_transform, "Sundial Frame")
 
     def get_mural_angle(self):
+        """
+        getter for the mural angle
+
+        Returns
+        -------
+        float
+            Mural angle.
+
+        """
         return self.mural_angle
 
     def get_height(self):
@@ -59,48 +93,47 @@ class MuralSundial(Sundial):
     def get_sundial_frame(self):
         return self.sundial_frame
 
-    def get_sun_pos_sundial_frame(self, UTCdate):
+    def get_sun_pos_sundial_frame(self, utc_date):
         j2000 = FramesFactory.getEME2000()
-        sunPosInJ2000 = self.pvSun.getPVCoordinates(UTCdate, j2000).getPosition()
-        j2000_to_sundial = j2000.getTransformTo(self.sundial_frame, UTCdate)
-        sun_pos_sundial_frame = j2000_to_sundial.transformPosition(sunPosInJ2000)
+        sun_pos_j2000 = self.pv_sun.getPVCoordinates(utc_date, j2000).getPosition()
+        j2000_to_sundial = j2000.getTransformTo(self.sundial_frame, utc_date)
+        sun_pos_sundial_frame = j2000_to_sundial.transformPosition(sun_pos_j2000)
         return sun_pos_sundial_frame
 
-    def get_sun_elevation_sundial_frame(self, UTCdate):
+    def get_sun_elevation_sundial_frame(self, utc_date):
         #Radians
-        return get_elevation(self.get_sun_pos_sundial_frame(UTCdate))
+        return get_elevation(self.get_sun_pos_sundial_frame(utc_date))
 
-    def get_sun_elevation_sundial_frame_degrees(self, UTCdate):
+    def get_sun_elevation_sundial_frame_degrees(self, utc_date):
         #Degrees
-        return degrees(get_elevation(self.get_sun_pos_sundial_frame(UTCdate)))
+        return degrees(get_elevation(self.get_sun_pos_sundial_frame(utc_date)))
 
-    def get_gnomon_shadow_length(self, UTCdate):
-        return self.gnomonLength/tan(self.get_sun_elevation_sundial_frame_degrees(UTCdate))
+    def get_gnomon_shadow_length(self, utc_date):
+        return self.gnomon_length/tan(self.get_sun_elevation_sundial_frame_degrees(utc_date))
 
-    def get_gnomon_shadow_azimuth(self, UTCdate):
-        return get_azimuth(self.get_sun_pos_sundial_frame(UTCdate))
+    def get_gnomon_shadow_azimuth(self, utc_date):
+        return get_azimuth(self.get_sun_pos_sundial_frame(utc_date))
 
-    def get_gnomon_shadow_azimuth_degrees(self, UTCdate):
-        return degrees(get_azimuth(self.get_sun_pos_sundial_frame(UTCdate)))
+    def get_gnomon_shadow_azimuth_degrees(self, utc_date):
+        return degrees(get_azimuth(self.get_sun_pos_sundial_frame(utc_date)))
 
-    def get_gnomon_shadow_angleWRTX(self, UTCdate):
-        return 3*pi/2 - self.get_gnomon_shadow_azimuth(UTCdate)
+    def get_gnomon_shadow_angle_wrtx(self, utc_date):
+        return 3*pi/2 - self.get_gnomon_shadow_azimuth(utc_date)
 
-    def get_gnomon_shadow_angleWRTX_degrees(self, UTCdate):
-        return degrees(3*pi/2 - self.get_gnomon_shadow_azimuth(UTCdate))
+    def get_gnomon_shadow_angle_wrtx_degrees(self, utc_date):
+        return degrees(3*pi/2 - self.get_gnomon_shadow_azimuth(utc_date))
 
     def get_gnomon_shadow_angle_zenith(self, year, month, day):
-        UTCdate = self.getZenithUTC(year, month, day)
-        return self.get_gnomon_shadow_angleWRTX(UTCdate)
+        utc_date = self.get_zenith_utc(year, month, day)
+        return self.get_gnomon_shadow_angle_wrtx(utc_date)
 
     def get_gnomon_shadow_angle_zenith_degrees(self, year, month, day):
-        UTCdate = self.getZenithUTC(year, month, day)
-        return self.get_gnomon_shadow_angleWRTX_degrees(UTCdate)
+        utc_date = self.get_zenith_utc(year, month, day)
+        return self.get_gnomon_shadow_angle_wrtx_degrees(utc_date)
 
     def get_all_gnomon_shadow_angle_degrees(self, year, month, day):
-        zenithUTC = self.getZenithUTC(year, month, day)
-        gnomonShadowAngles = [degrees(self.get_gnomon_shadow_angle_zenith(year, month, day))]
-        # assert self.getGnomonShadowAngleZenith(year, month, day) == self.getGnomonShadowAngleWRTX(zenithUTC)
+        zenith_utc = self.get_zenith_utc(year, month, day)
+        gnomon_shadow_angles = [degrees(self.get_gnomon_shadow_angle_zenith(year, month, day))]
         for i in range(1, 12):
-            gnomonShadowAngles.append(self.get_gnomon_shadow_angleWRTX_degrees(zenithUTC.shiftedBy(i*3600.0)))
-        return gnomonShadowAngles
+            gnomon_shadow_angles.append(self.get_gnomon_shadow_angle_wrtx_degrees(zenith_utc.shiftedBy(i*3600.0)))
+        return gnomon_shadow_angles
